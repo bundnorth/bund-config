3324ee655e3dc9567b10b73acdef93a7236fe850
yougui.xiong <yougui.xiong@zhangmen.com>
2020-02-22 23:57:09 +0800

    add eureka

--- src/main/java/com/bund/north/bund/config/BundConfigApplication.java
+++ src/main/java/com/bund/north/bund/config/BundConfigApplication.java
@@ -3,7 +3,9 @@ package com.bund.north.bund.config;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Hugo
@@ -11,6 +13,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 @EnableEncryptableProperties
+//@EnableEurekaClient
+@EnableDiscoveryClient
 public class BundConfigApplication {
 
 	public static void main(String[] args) {
